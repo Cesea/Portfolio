7d3e12746488dae2d67493513e0996fe7a09b617
ILH <cwh9141@naver.com>
2017-11-19 20:30:30 +0900

    struct 함수
    collisionComponent의 struct에 초기화 함수와 operator 삽입

--- Hunter/CollisionComponent.h
+++ Hunter/CollisionComponent.h
@@ -5,18 +5,42 @@
 
 struct BoundingSphere
 {
-	Vector3 _localCenter;
-	float _radius;
-	Vector3 _halfSize;
+	Vector3 localCenter;
+	float radius;
+	BoundingSphere & operator=(const BoundingSphere & other)
+	{
+		this->localCenter = other.localCenter;
+		this->radius = other.radius;
+	}
 };
 
 struct BoundingBox
 {
-	Vector3 _localCenter;
-	float _radius;
-	Vector3 _halfSize;
+	Vector3 localCenter;
+	float xSize;
+	float ySize;
+	float zSize;
 	Vector3 localMinPos;
 	Vector3 localMaxPos;
+	void initialBox(const Vector3 & min, const Vector3 & max)
+	{
+		localMinPos = min;
+		localMaxPos = max;
+		xSize = (max.x - min.x) / 2;
+		ySize = (max.y - min.y) / 2;
+		zSize = (max.z - min.z) / 2;
+		localCenter = (min + max) / 2;
+	}
+
+	BoundingBox & operator=(const BoundingBox & other)
+	{
+		this->localCenter = other.localCenter;
+		this->xSize = other.xSize;
+		this->ySize = other.ySize;
+		this->zSize = other.zSize;
+		this->localMinPos = other.localMaxPos;
+		this->localMaxPos = other.localMaxPos;
+	}
 };
 
 struct CollisionComponent : public Component
