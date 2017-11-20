ab739ada4592ce64c0c9f9757f2e2aff9aaef842
ILH <cwh9141@naver.com>
2017-11-20 18:57:11 +0900

    fMod 추가

--- Hunter/Enemy.h
+++ Hunter/Enemy.h
@@ -5,8 +5,13 @@ class Enemy
 	enum stateTag
 	{
 		IDLE,
+		FIND,
 		RUN,
-		ATK,
+		ATK1,
+		ATK2,
+		ATK3,
+		ATK4,
+		ATK5,
 		HURT,
 		DIE
 	};
@@ -14,6 +19,7 @@ protected:
 	float _hp;
 	float _speed;
 	stateTag _state;
+	std::vector<Vector3> _moveSegment;
 public:
 	virtual bool CreateFromWorld(World &world) = 0;
 	virtual void update(float deltaTime) = 0;
